7e8c4d06a033b9851d1b88e16b59d90d84a1eb82
Joy Adigwe <joyadigwe7@gmail.com>
2022-08-07 12:39:23 -0500

    more leetcode

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ function hello () {
 function addition(a,b) {
     return a + b;
 }
+
+//Create a function that takes an array containing only numbers and return the first element.
+function getFirstValue(arr) {
+    return arr[0];
+}
+
+/*Two Sum
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that ecah input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order */
+
+var twoSum = function(nums, target) {
+    for (var i=0; i<nums.length; i++) {
+        for (var j= i +1; j < nums.length; j++) {
+            if(nums[i] + nums[j] === target) {
+                return[i, j];
+            }
+        }
+    }
+};
