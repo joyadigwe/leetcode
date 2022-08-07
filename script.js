d3a16a12dad731f7efded9a9c4f3a5e797e8602e
Joy Adigwe <joyadigwe7@gmail.com>
2022-08-07 11:36:13 -0500

    edabit challenges

---
+++ script.js
@@ -0,0 +1,8 @@
+function hello () {
+    return "hello edabit.com"
+}
+
+/* Create a function that takes two numbers as arguments and returns their sum */
+function addition(a,b) {
+    return a + b;
+}
