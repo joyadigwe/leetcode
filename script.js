1fb7855b5c46216863556a51661dae304ad62a89
Joy Adigwe <joyadigwe7@gmail.com>
2022-08-09 09:41:41 -0500

    more problems

--- script.js
+++ script.js
@@ -27,4 +27,30 @@ var twoSum = function(nums, target) {
     }
 };
 
-const convert = (minutes) => minutes * 60
+
+
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4];
+//Check for the first reoccuring element
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]
+//It should return 1
+
+//Given an array = [2,3,4,5];
+//It should return undefined
+
+
+//brute force approach
+function firstRecurringCharacter(input) {
+    for(let i = 0; i < input.length; i++){
+        //every time outer for loop shifts then j will still be reset to 1 so you need to make it i + 1
+        for(let j = i + 1; j < input.length; j++) {
+            if (input[i] === input[j]) {
+                return input[i]
+            }
+        }
+    }
+    return undefined;
+}
+firstRecurringCharacter([2,5,1,2,3,5,1,2,4])
