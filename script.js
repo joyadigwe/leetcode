51fbd353173c9b547e65dbcad7b12bdfda786d88
Joy Adigwe <joyadigwe7@gmail.com>
2022-08-16 12:51:12 -0500

    linked lists

--- script.js
+++ script.js
@@ -105,3 +105,41 @@ function palindrome(str) {
     }
 }
 console.log(palindrome("eye"));
+
+//Linked Lists
+//10-->5-->16
+
+/* let myLinkedList = {
+    //object with head property
+    head: {
+        //node with data 
+        value: 10,
+        next: {
+            value: 5, 
+            next: {
+                value: 16,
+                next: null
+            }
+        }
+    }
+}*/
+
+class LinkedList {
+    //constructor is about creating the very first linked list
+    constructor(value) {
+    this.head = {
+        value: value,
+        next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+    }
+    append(value) {
+        //Code here
+    }
+}
+
+const myLinkedList = new LinkedList(10);
+//myLinkedList.append(5);
+//myLinkedList.append(16);
+console.log(myLinkedList);
